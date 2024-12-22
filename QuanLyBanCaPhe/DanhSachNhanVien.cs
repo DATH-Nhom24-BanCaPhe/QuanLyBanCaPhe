437d4bc29b6170c6e717f3bdaa3b0eb19a9c9b95
DATH-Nhom24-BanCaPhe <tranngoctrucly1674@gmail.com>
2024-12-23 01:58:40 +0700

    Update DanhSachNhanVien.cs

--- QuanLyBanCaPhe/DanhSachNhanVien.cs
+++ QuanLyBanCaPhe/DanhSachNhanVien.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QuanLyBanCaPhe
 {
@@ -25,22 +26,35 @@ namespace QuanLyBanCaPhe
                 get { return dsNV; }
                 set { dsNV = value; }
             }
-            public bool kiemTraMa(string ID)
-            {
-
-                string query = "SELECT COUNT(*) FROM nhanVien WHERE maNV = @maNV";
-                SqlCommand command = new SqlCommand(query, conn);
-                command.Parameters.AddWithValue("@maNV", ID);
-
-                int count = (int)command.ExecuteScalar();
-                return count > 0;
-            }
-            public bool Them(QuanLyNhanVien nv)
-            {
-                if (kiemTraMa(nv.ID))
-                    return false;
-
+            public void Them(QuanLyNhanVien nv)
+            {                  
                 string query = "INSERT INTO nhanVien(maNV, hoTen, ngaySinh, sĐT, ngayVaoLam, viTriLamViec, gioiTinh) VALUES (@maNV, @hoTen, @ngaySinh, @sĐT, @ngayVaoLam, @viTriLamViec, @gioiTinh)";
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@maNV", nv.ID);
+                command.Parameters.AddWithValue("@hoTen", nv.HoTen);
+                command.Parameters.AddWithValue("@ngaySinh", nv.NgaySinh);
+                command.Parameters.AddWithValue("@sĐT", nv.SĐT);
+                command.Parameters.AddWithValue("@ngayVaoLam", nv.NgayVaoLam);
+                command.Parameters.AddWithValue("@viTriLamViec", nv.ViTriLamViec);
+                command.Parameters.AddWithValue("@gioiTinh", nv.GioiTinh);
+                command.ExecuteNonQuery();
+                this.dsNV.Add(nv);       
+            MessageBox.Show("Thêm thông tin thành công !","Thông báo",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+            public void Xoa(string maNV)
+            {
+                string query = "DELETE FROM nhanVien WHERE maNV = @maNV";
+                SqlCommand command = new SqlCommand(query, conn);
+
+                command.Parameters.AddWithValue("@maNV", maNV);
+                command.ExecuteNonQuery();
+                dsNV.RemoveAll(nv => nv.ID == maNV);
+            MessageBox.Show("Xóa thành công", "Thông báo", MessageBoxButtons.OK);
+
+        }
+            public void Sua(QuanLyNhanVien nv)
+            {
+                string query = "UPDATE nhanVien SET hoTen = @hoTen, ngaySinh = @ngaySinh, sĐT = @sĐT, ngayVaoLam = @ngayVaoLam, viTriLamViec = @viTriLamViec, gioiTinh = @gioiTinh WHERE maNV = @maNV";
                 SqlCommand command = new SqlCommand(query, conn);
 
                 command.Parameters.AddWithValue("@maNV", nv.ID);
@@ -52,48 +66,19 @@ namespace QuanLyBanCaPhe
                 command.Parameters.AddWithValue("@gioiTinh", nv.GioiTinh);
 
                 command.ExecuteNonQuery();
-                this.dsNV.Add(nv);
-                return true;
-
-            }
-            public bool Xoa(string maNV)
-            {
-                string query = "DELETE FROM nhanVien WHERE maNV = @maNV";
-                SqlCommand command = new SqlCommand(query, conn);
-
-                command.Parameters.AddWithValue("@maNV", maNV);
-                command.ExecuteNonQuery();
-                dsNV.RemoveAll(nv => nv.ID == maNV);
-                return true;
-
-            }
-            public bool Sua(QuanLyNhanVien nv)
-            {
-                string query = "UPDATE nhanVien SET hoTen = @hoTen, ngaySinh = @ngaySinh, sDT = @sDT, ngayVaoLam = @ngayVaoLam, viTriLamViec = @viTriLamViec, gioiTinh = @gioiTinh WHERE maNV = @maNV";
-                SqlCommand command = new SqlCommand(query, conn);
-
-                command.Parameters.AddWithValue("@maNV", nv.ID);
-                command.Parameters.AddWithValue("@hoTen", nv.HoTen);
-                command.Parameters.AddWithValue("@ngaySinh", nv.NgaySinh);
-                command.Parameters.AddWithValue("@sDT", nv.SĐT);
-                command.Parameters.AddWithValue("@ngayVaoLam", nv.NgayVaoLam);
-                command.Parameters.AddWithValue("@viTriLamViec", nv.ViTriLamViec);
-                command.Parameters.AddWithValue("@gioiTinh", nv.GioiTinh);
-
-                command.ExecuteNonQuery();
 
                 int index = dsNV.FindIndex(n => n.ID == nv.ID);
                 if (index != -1)
                 {
                     dsNV[index] = nv;
                 }
-                return true;
+            MessageBox.Show("Cập nhật dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
 
-            }
+        }
             public List<QuanLyNhanVien> TimTheoMa(string ma)
             {
                 List<QuanLyNhanVien> ketQua = new List<QuanLyNhanVien>();
-                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = doan; Integrated Security = True;");
+                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("select * from nhanVien where maNV=@maNV", conn);
                 cmd.Parameters.AddWithValue("@maNV", ma);
@@ -117,7 +102,7 @@ namespace QuanLyBanCaPhe
             public List<QuanLyNhanVien> TimTheoTen(string ten)
             {
                 List<QuanLyNhanVien> ketQua = new List<QuanLyNhanVien>();
-                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = doan; Integrated Security = True;");
+                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("select * from nhanVien where hoTen like '%'+@hoTen+'%'", conn);
                 cmd.Parameters.AddWithValue("@hoTen", ten);

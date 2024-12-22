db329679bc074e1c07d1062b3fba9ab1fc1ff661
DATH-Nhom24-BanCaPhe <tranngoctrucly1674@gmail.com>
2024-12-23 01:58:12 +0700

    Update DanhSachLoaiNuoc.cs

--- QuanLyBanCaPhe/DanhSachLoaiNuoc.cs
+++ QuanLyBanCaPhe/DanhSachLoaiNuoc.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QuanLyBanCaPhe
 {
@@ -36,55 +37,48 @@ namespace QuanLyBanCaPhe
                 int count = (int)command.ExecuteScalar();
                 return count > 0;
             }
-            public bool Them(QuanLyLoaiNuoc ln)
+            public void Them(QuanLyLoaiNuoc ln)
             {
                 if (kiemTraMa(ln.MaLoai))
-                    return false;
-
+            {
+                MessageBox.Show("Trùng mã!.Vui lòng nhập mã khác!", "Thông báo", MessageBoxButtons.OK);
+                return;
+            }      
                 string them = "insert into LoaiNuoc(maLoai,tenLoai) values(@maLoai,@tenLoai)";
                 SqlCommand command = new SqlCommand(them, conn);
-
                 command.Parameters.AddWithValue("@maLoai", ln.MaLoai);
                 command.Parameters.AddWithValue("@tenLoai", ln.TenLoai);
-
-
                 command.ExecuteNonQuery();
                 this.dsLN.Add(ln);
-                return true;
-
+            MessageBox.Show("Thêm dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
             }
-            public bool Xoa(string maLoai)
+            public void Xoa(string maLoai)
             {
-                string query = "DELETE FROM LoaiNuoc WHERE maNuoc = @maNuoc";
+                string query = "DELETE FROM LoaiNuoc WHERE maLoai=@maLoai";
                 SqlCommand command = new SqlCommand(query, conn);
-
                 command.Parameters.AddWithValue("@maLoai", maLoai);
                 command.ExecuteNonQuery();
                 dsLN.RemoveAll(ln => ln.MaLoai == maLoai);
-                return true;
+                MessageBox.Show("Xóa thành công !","Thông báo",MessageBoxButtons.OK);
             }
-
-            public bool Sua(QuanLyLoaiNuoc ln)
+            public void Sua(QuanLyLoaiNuoc ln)
             {
-                string query = "UPDATE LoaiNuoc SET maLoai=@maLoai,tenLoai=@tenLoai";
+                string query = "Update LoaiNuoc SET tenLoai=@tenLoai where maLoai=@maLoai";
                 SqlCommand command = new SqlCommand(query, conn);
                 command.Parameters.AddWithValue("@maLoai", ln.MaLoai);
                 command.Parameters.AddWithValue("@tenLoai", ln.TenLoai);
-
                 command.ExecuteNonQuery();
-
                 int index = dsLN.FindIndex(n => n.MaLoai == ln.MaLoai);
                 if (index != -1)
                 {
                     dsLN[index] = ln;
                 }
-                return true;
-
+            MessageBox.Show("Cập nhật dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
             }
             public List<QuanLyLoaiNuoc> TimTheoMa(string ma)
             {
                 List<QuanLyLoaiNuoc> ketQua = new List<QuanLyLoaiNuoc>();
-                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = doan; Integrated Security = True;");
+                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("select * from LoaiNuoc where maLoai=@maLoai", conn);
                 cmd.Parameters.AddWithValue("@maLoai", ma);
@@ -105,9 +99,9 @@ namespace QuanLyBanCaPhe
             public List<QuanLyLoaiNuoc> TimTheoTen(string ten)
             {
                 List<QuanLyLoaiNuoc> ketQua = new List<QuanLyLoaiNuoc>();
-                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = doan; Integrated Security = True;");
+                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("select * from nhanVien where tenLoai like '%'+@tenLoai+'%'", conn);
+                SqlCommand cmd = new SqlCommand("select * from LoaiNuoc where tenLoai like '%'+@tenLoai+'%'", conn);
                 cmd.Parameters.AddWithValue("@tenLoai", ten);
 
                 SqlDataReader reader = cmd.ExecuteReader();

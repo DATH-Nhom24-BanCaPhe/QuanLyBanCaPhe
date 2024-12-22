5779fc4eb8ea8bd42e7d0967a75fd1df5c267bf3
DATH-Nhom24-BanCaPhe <tranngoctrucly1674@gmail.com>
2024-12-23 02:03:38 +0700

    Update DanhSachNhanVien.cs

--- QuanLyBanCaPhe/DanhSachNhanVien.cs
+++ QuanLyBanCaPhe/DanhSachNhanVien.cs
@@ -8,121 +8,136 @@ using System.Windows.Forms;
 
 namespace QuanLyBanCaPhe
 {
-    internal class DanhSachSanPham
+    internal class DanhSachNhanVien
     {
-            private List<QuanLySanPham> dsDU;
+            private List<QuanLyNhanVien> dsNV;
             private SqlConnection conn;
-            public DanhSachSanPham(SqlConnection conn)
+            public DanhSachNhanVien(SqlConnection conn)
             {
-                this.dsDU = new List<QuanLySanPham>();
+                this.dsNV = new List<QuanLyNhanVien>();
                 this.conn = conn;
             }
-            public DanhSachSanPham(List<QuanLySanPham> dsDU)
+            public DanhSachNhanVien(List<QuanLyNhanVien> dsNV)
             {
-                this.dsDU = dsDU;
+                this.dsNV = dsNV;
             }
-            public List<QuanLySanPham> DSDU
+            public List<QuanLyNhanVien> DsNV
             {
-                get { return this.dsDU; }
-                set { this.dsDU = value; }
+                get { return dsNV; }
+                set { dsNV = value; }
             }
-            public bool kiemTraMa(string maNuoc)
+            public void Them(QuanLyNhanVien nv)
+            {                  
+                string query = "INSERT INTO nhanVien(maNV, hoTen, ngaySinh, sĐT, ngayVaoLam, viTriLamViec, gioiTinh) VALUES (@maNV, @hoTen, @ngaySinh, @sĐT, @ngayVaoLam, @viTriLamViec, @gioiTinh)";
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@maNV", nv.ID);
+                command.Parameters.AddWithValue("@hoTen", nv.HoTen);
+                command.Parameters.AddWithValue("@ngaySinh", nv.NgaySinh);
+                command.Parameters.AddWithValue("@sĐT", nv.SĐT);
+                command.Parameters.AddWithValue("@ngayVaoLam", nv.NgayVaoLam);
+                command.Parameters.AddWithValue("@viTriLamViec", nv.ViTriLamViec);
+                command.Parameters.AddWithValue("@gioiTinh", nv.GioiTinh);
+                command.ExecuteNonQuery();
+                this.dsNV.Add(nv);       
+            MessageBox.Show("Thêm thông tin thành công !","Thông báo",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+            public void Xoa(string maNV)
             {
+                string query = "DELETE FROM nhanVien WHERE maNV = @maNV";
+                SqlCommand command = new SqlCommand(query, conn);
 
-                string query = "SELECT COUNT(*) FROM doUong WHERE maNuoc = @maNuoc";
-                SqlCommand command = new SqlCommand(query, conn);
-                command.Parameters.AddWithValue("@maNuoc", maNuoc);
+                command.Parameters.AddWithValue("@maNV", maNV);
+                command.ExecuteNonQuery();
+                dsNV.RemoveAll(nv => nv.ID == maNV);
+            MessageBox.Show("Xóa thành công", "Thông báo", MessageBoxButtons.OK);
 
-                int count = (int)command.ExecuteScalar();
-                return count > 0;
-            }
-        public void Them(QuanLySanPham du)
-        {
-            if (kiemTraMa(du.MaNuoc)) { 
-                MessageBox.Show("Trùng mã.Vui lòng nhập mã khác!", "Thông báo", MessageBoxButtons.OK);
-            return;
         }
-                string them = "insert into doUong(maLoai,maNuoc,tenNuoc,gia) values(@maLoai,@maNuoc,@tenNuoc,@gia)";
-                SqlCommand command = new SqlCommand(them, conn);
-                command.Parameters.AddWithValue("@maLoai", du.MaLoai);
-                command.Parameters.AddWithValue("@maNuoc", du.MaNuoc);
-                command.Parameters.AddWithValue("@tenNuoc", du.TenNuoc);
-                command.Parameters.AddWithValue("@gia", du.Gia);
-                command.ExecuteNonQuery();
-                this.dsDU.Add(du);
-            MessageBox.Show("Thêm dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
-        }
-            public void Xoa(string maNuoc)
+            public void Sua(QuanLyNhanVien nv)
             {
-                string query = "DELETE FROM doUong WHERE maNuoc = @maNuoc ";
+                string query = "UPDATE nhanVien SET hoTen = @hoTen, ngaySinh = @ngaySinh, sĐT = @sĐT, ngayVaoLam = @ngayVaoLam, viTriLamViec = @viTriLamViec, gioiTinh = @gioiTinh WHERE maNV = @maNV";
                 SqlCommand command = new SqlCommand(query, conn);
-                command.Parameters.AddWithValue("@maNuoc", maNuoc);
+
+                command.Parameters.AddWithValue("@maNV", nv.ID);
+                command.Parameters.AddWithValue("@hoTen", nv.HoTen);
+                command.Parameters.AddWithValue("@ngaySinh", nv.NgaySinh);
+                command.Parameters.AddWithValue("@sĐT", nv.SĐT);
+                command.Parameters.AddWithValue("@ngayVaoLam", nv.NgayVaoLam);
+                command.Parameters.AddWithValue("@viTriLamViec", nv.ViTriLamViec);
+                command.Parameters.AddWithValue("@gioiTinh", nv.GioiTinh);
+
                 command.ExecuteNonQuery();
-                dsDU.RemoveAll(du => du.MaNuoc == maNuoc);
-                MessageBox.Show("Xóa dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
-        }
-            public void Sua(QuanLySanPham du)
-            {
-                string query = "UPDATE doUong SET tenNuoc=@tenNuoc,gia=@gia where maNuoc=@maNuoc";
-                SqlCommand command = new SqlCommand(query, conn);
-                command.Parameters.AddWithValue("@mLoai", du.MaLoai);
-                command.Parameters.AddWithValue("@maNuoc", du.MaNuoc);
-                command.Parameters.AddWithValue("@tenNuoc", du.TenNuoc);
-                command.Parameters.AddWithValue("@gia", du.Gia);
-                command.ExecuteNonQuery();
-                int index = dsDU.FindIndex(n => n.MaNuoc == du.MaNuoc);
+
+                int index = dsNV.FindIndex(n => n.ID == nv.ID);
                 if (index != -1)
                 {
-                    dsDU[index] = du;
+                    dsNV[index] = nv;
                 }
-            MessageBox.Show("Thông tin đã được cập nhật!", "Thông báo", MessageBoxButtons.OK);
+            MessageBox.Show("Cập nhật dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
+
         }
-            public List<QuanLySanPham> TimTheoTen(string ten)
+            public List<QuanLyNhanVien> TimTheoMa(string ma)
             {
-                List<QuanLySanPham> ketQua = new List<QuanLySanPham>();
+                List<QuanLyNhanVien> ketQua = new List<QuanLyNhanVien>();
                 SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("select * from doUong where tenNuoc like '%'+@tenNuoc+'%'", conn);
-                cmd.Parameters.AddWithValue("@tenNuoc", ten);
+                SqlCommand cmd = new SqlCommand("select * from nhanVien where maNV=@maNV", conn);
+                cmd.Parameters.AddWithValue("@maNV", ma);
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    QuanLySanPham du = new QuanLySanPham
+                    QuanLyNhanVien nv = new QuanLyNhanVien
                     {
-                        MaLoai = reader["maLoai"].ToString(),
-                        MaNuoc = reader["maNuoc"].ToString(),
-                        TenNuoc = reader["tenNuoc"].ToString(),
-                        Gia = Convert.ToDouble(reader["gia"])
-
+                        ID = reader["maNV"].ToString(),
+                        HoTen = reader["hoTen"].ToString(),
+                        NgaySinh = Convert.ToDateTime(reader["ngaySinh"]),
+                        SĐT = reader["sĐT"].ToString(),
+                        NgayVaoLam = Convert.ToDateTime(reader["ngayVaoLam"]),
+                        ViTriLamViec = reader["viTriLamViec"].ToString(),
+                        GioiTinh = reader["gioiTinh"].ToString()
                     };
-                    ketQua.Add(du);
+                    ketQua.Add(nv);
                 }
                 return ketQua;
             }
-            public List<QuanLySanPham> TimTheoMa(string ma)
+            public List<QuanLyNhanVien> TimTheoTen(string ten)
             {
-                List<QuanLySanPham> ketQua = new List<QuanLySanPham>();
+                List<QuanLyNhanVien> ketQua = new List<QuanLyNhanVien>();
                 SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("select * from doUong where maNuoc=@maNuoc", conn);
-                cmd.Parameters.AddWithValue("@maNuoc", ma);
+                SqlCommand cmd = new SqlCommand("select * from nhanVien where hoTen like '%'+@hoTen+'%'", conn);
+                cmd.Parameters.AddWithValue("@hoTen", ten);
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    QuanLySanPham du = new QuanLySanPham
+                    QuanLyNhanVien nv = new QuanLyNhanVien
                     {
-                        MaLoai = reader["maLoai"].ToString(),
-                        MaNuoc = reader["maNuoc"].ToString(),
-                        TenNuoc = reader["tenNuoc"].ToString(),
-                        Gia = Convert.ToDouble(reader["gia"])
-
+                        ID = reader["maNV"].ToString(),
+                        HoTen = reader["hoTen"].ToString(),
+                        NgaySinh = Convert.ToDateTime(reader["ngaySinh"]),
+                        SĐT = reader["sĐT"].ToString(),
+                        NgayVaoLam = Convert.ToDateTime(reader["ngayVaoLam"]),
+                        ViTriLamViec = reader["viTriLamViec"].ToString(),
+                        GioiTinh = reader["gioiTinh"].ToString()
                     };
-                    ketQua.Add(du);
+                    ketQua.Add(nv);
                 }
                 return ketQua;
             }
+            public int tinhTuoi(DateTime ngaySinh)
+            {
+                DateTime ngayHienTai = DateTime.Now;
+                int tuoi = ngayHienTai.Year - ngaySinh.Year;
+                if (ngayHienTai < ngaySinh)
+                {
+                    return -1;
+                }
+                if (ngayHienTai.Month < ngaySinh.Month || (ngayHienTai.Month == ngaySinh.Month && ngayHienTai.Day < ngaySinh.Day))
+                {
+                    tuoi--;
+                }
+                return tuoi;
+            }
+
         }
     }
-
-
-
+    

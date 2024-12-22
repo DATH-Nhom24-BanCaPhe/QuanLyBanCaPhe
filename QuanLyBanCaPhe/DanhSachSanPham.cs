0c6bf8d5151d0523997a8e83c932ac6251287683
DATH-Nhom24-BanCaPhe <tranngoctrucly1674@gmail.com>
2024-12-23 01:59:13 +0700

    Update DanhSachSanPham.cs

--- QuanLyBanCaPhe/DanhSachSanPham.cs
+++ QuanLyBanCaPhe/DanhSachSanPham.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QuanLyBanCaPhe
 {
@@ -35,55 +36,51 @@ namespace QuanLyBanCaPhe
                 int count = (int)command.ExecuteScalar();
                 return count > 0;
             }
-            public bool Them(QuanLySanPham du)
-            {
-                if (kiemTraMa(du.MaNuoc))
-                    return false;
-
-                string them = "insert into doUong(maNuoc,tenNuoc,gia) values(@maNuoc,@tenNuoc,@gia)";
+        public void Them(QuanLySanPham du)
+        {
+            if (kiemTraMa(du.MaNuoc)) { 
+                MessageBox.Show("Trùng mã.Vui lòng nhập mã khác!", "Thông báo", MessageBoxButtons.OK);
+            return;
+        }
+                string them = "insert into doUong(maLoai,maNuoc,tenNuoc,gia) values(@maLoai,@maNuoc,@tenNuoc,@gia)";
                 SqlCommand command = new SqlCommand(them, conn);
-
+                command.Parameters.AddWithValue("@maLoai", du.MaLoai);
                 command.Parameters.AddWithValue("@maNUoc", du.MaNuoc);
                 command.Parameters.AddWithValue("@tenNuoc", du.TenNuoc);
                 command.Parameters.AddWithValue("@gia", du.Gia);
-
                 command.ExecuteNonQuery();
                 this.dsDU.Add(du);
-                return true;
-
-            }
-            public bool Xoa(string maNuoc)
+            MessageBox.Show("Thêm dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
+        }
+            public void Xoa(string maNuoc)
             {
-                string query = "DELETE FROM doUong WHERE maNuoc = @maNuoc";
+                string query = "DELETE FROM doUong WHERE maNuoc = @maNuoc ";
                 SqlCommand command = new SqlCommand(query, conn);
-
                 command.Parameters.AddWithValue("@maNuoc", maNuoc);
                 command.ExecuteNonQuery();
                 dsDU.RemoveAll(du => du.MaNuoc == maNuoc);
-                return true;
-            }
-            public bool Sua(QuanLySanPham du)
+                MessageBox.Show("Xóa dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
+        }
+            public void Sua(QuanLySanPham du)
             {
-                string query = "UPDATE doUong SET maNuoc=@maNuoc,tenNuoc=@tenNuoc,gia=@gia";
+                string query = "UPDATE doUong SET tenNuoc=@tenNuoc,gia=@gia where maNuoc=@maNuoc";
                 SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@mLoai", du.MaLoai);
                 command.Parameters.AddWithValue("@maNUoc", du.MaNuoc);
                 command.Parameters.AddWithValue("@tenNuoc", du.TenNuoc);
                 command.Parameters.AddWithValue("@gia", du.Gia);
-
                 command.ExecuteNonQuery();
-
                 int index = dsDU.FindIndex(n => n.MaNuoc == du.MaNuoc);
                 if (index != -1)
                 {
                     dsDU[index] = du;
                 }
-                return true;
-
-            }
+            MessageBox.Show("Thông tin đã được cập nhật!", "Thông báo", MessageBoxButtons.OK);
+        }
             public List<QuanLySanPham> TimTheoTen(string ten)
             {
                 List<QuanLySanPham> ketQua = new List<QuanLySanPham>();
-                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = doan; Integrated Security = True;");
+                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("select * from doUong where tenNuoc like '%'+@tenNuoc+'%'", conn);
                 cmd.Parameters.AddWithValue("@tenNuoc", ten);
@@ -105,7 +102,7 @@ namespace QuanLyBanCaPhe
             public List<QuanLySanPham> TimTheoMa(string ma)
             {
                 List<QuanLySanPham> ketQua = new List<QuanLySanPham>();
-                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = doan; Integrated Security = True;");
+                SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("select * from doUong where maNuoc=@maNuoc", conn);
                 cmd.Parameters.AddWithValue("@maNuoc", ma);

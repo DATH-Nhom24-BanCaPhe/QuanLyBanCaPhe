b3f3f7d51b07c060d997125c00cc4e297189e860
DATH-Nhom24-BanCaPhe <tranngoctrucly1674@gmail.com>
2024-12-23 01:57:26 +0700

    Update DanhSachCTHĐ.cs

--- QuanLyBanCaPhe/DanhSachCTHĐ.cs
+++ QuanLyBanCaPhe/DanhSachCTHĐ.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QuanLyBanCaPhe
 {
@@ -29,50 +30,71 @@ namespace QuanLyBanCaPhe
             }
             public bool Them(QuanLyCTHĐ ct)
             {
-                string them = "insert into CTHĐ(maHD,maNuoc,tenNuoc,soLuong,gia,@thanhTien) values(@maHD,@maNuoc,@tenNuoc,@soLuong,@thanhTien)";
+                string them = "insert into CTHĐ(maHD,maNuoc,soLuong,gia,@thanhTien,tenNuoc) values(@maHD,@maNuoc,@soLuong,@thanhTien,@tenNuoc)";
                 SqlCommand command = new SqlCommand(them, conn);
                 command.Parameters.AddWithValue("@maHD", ct.MaHD);
                 command.Parameters.AddWithValue("@maNuoc", ct.MaNuoc);
-                command.Parameters.AddWithValue("@tenNuoc", ct.TenNuoc);
                 command.Parameters.AddWithValue("@soLuong", ct.SoLuong);
                 command.Parameters.AddWithValue("@gia", ct.Gia);
                 command.Parameters.AddWithValue("@thanhTien",ct.ThanhTien);
+                command.Parameters.AddWithValue("@tenNuoc", ct.TenNuoc);
                 command.ExecuteNonQuery();
                 this.dsCTHĐ.Add(ct);
                 return true;
             }
-            public bool Xoa(string maHD)
+            public void Xoa(string maHD)
             {
-
-                string query = "DELETE FROM hoaDon WHERE maHD = @maHD";
+                string query = "delete from CTHĐ where maHD=@maHD delete from hoaDon where maHD=@maHD";
                 SqlCommand command = new SqlCommand(query, conn);
-
                 command.Parameters.AddWithValue("@maHD", maHD);
                 command.ExecuteNonQuery();
                 dsCTHĐ.RemoveAll(ct => ct.MaHD == maHD);
-                return true;
+                MessageBox.Show("Xóa thành công !", "Thông báo", MessageBoxButtons.OK);
             }
-            public bool Sua(QuanLyCTHĐ ct)
+            public void Sua(QuanLyCTHĐ ct)
             {
-                string query = "UPDATE hoaDon SET maNuoc=@maNuoc,tenNuoc=@tenNuoc,soLuong=@soLuong,gia=@gia,thanhTien=@thanhTien where maHD=@maHD";
+                string query = "UPDATE hoaDon SET maNuoc=@maNuoc,soLuong=@soLuong,gia=@gia,thanhTien=@thanhTien,tenNuoc=@tenNuoc where maHD=@maHD";
                 SqlCommand command = new SqlCommand(query, conn);
                 command.Parameters.AddWithValue("@maHD", ct.MaHD);
-                command.Parameters.AddWithValue("@maNuoc", ct.MaNuoc);
-                command.Parameters.AddWithValue("@tenNuoc", ct.TenNuoc);
+                command.Parameters.AddWithValue("@maNuoc", ct.MaNuoc); 
                 command.Parameters.AddWithValue("@soLuong", ct.SoLuong);
                 command.Parameters.AddWithValue("@gia", ct.Gia);
-            command.Parameters.AddWithValue("@thanhTien", ct.ThanhTien);
-            command.ExecuteNonQuery();
+                command.Parameters.AddWithValue("@thanhTien", ct.ThanhTien);
+                command.Parameters.AddWithValue("@tenNuoc", ct.TenNuoc);
+                command.ExecuteNonQuery();
 
                 int index = dsCTHĐ.FindIndex(n => n.MaHD == ct.MaHD);
                 if (index != -1)
                 {
                     dsCTHĐ[index] = ct;
                 }
-                return true;
+            MessageBox.Show("Cập nhật dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
 
             }
-
+        public List<QuanLyCTHĐ> XemCTHĐ(string maHD)
+        {
+            List<QuanLyCTHĐ> ketQua = new List<QuanLyCTHĐ>();
+            SqlConnection conn = new SqlConnection("Data Source = TRUCLY; Initial Catalog = csdl; Integrated Security = True;");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from CTHĐ where maHD=@maHD", conn);
+            cmd.Parameters.AddWithValue("@maHD", maHD);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                QuanLyCTHĐ ct = new QuanLyCTHĐ
+                {
+                    MaHD = reader["maHD"].ToString(),
+                    MaNuoc = reader["maNuoc"].ToString(),
+                    TenNuoc = reader["tenNuoc"].ToString(),
+                    SoLuong = Convert.ToInt32(reader["soLuong"]),
+                    Gia = Convert.ToDouble(reader["gia"]),
+                    ThanhTien = Convert.ToDouble(reader["thanhTien"])
+                };
+                ketQua.Add(ct);
+            }
+            return ketQua;
         }
+
+    }
     }
 
